67a8c3773c4b8c8eca4dd69c829fdd6b848e9e1d
Manoj Chauhan <manoj@kiwitech.com>
2018-05-11 07:52:59 +0530

    appbar made not scrollable

--- src/Application.js
+++ src/Application.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import GridListExampleSimple from './GridListExampleSimple';
 import TextFieldExampleSimple from './TextFieldExampleSimple';
+import TextField from 'material-ui/TextField';
 
 class Application extends Component {
   render() {
     return (
-      <div>
-      <AppBar
-          title="Title"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-        />
-      <GridListExampleSimple/>
-      <img src={ require('./images/image1.jpeg') } />
-      <TextFieldExampleSimple/>
+      <div style={{ paddingTop: 56 }}>
+          <AppBar style={{ position: 'fixed', top: 0 }} />
+          <img src={ require('./images/banner.jpeg') } />
+          <TextField hintText="Hint Text"/>
+        <GridListExampleSimple/>
       </div>
     );
   }
---
+++ src/ReactGoogleMaps.js
@@ -0,0 +1,26 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import {
+//   withScriptjs,
+//   withGoogleMap,
+//   GoogleMap,
+//   Marker
+// } from "react-google-maps";
+//
+// const ReactGoogleMaps = compose(
+//   withProps({
+//     googleMapURL:
+//       "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+//     loadingElement: <div style={{ height: `100%` }} />,
+//     containerElement: <div style={{ height: `400px` }} />,
+//     mapElement: <div style={{ height: `100%` }} />
+//   }),
+//   withScriptjs,
+//   withGoogleMap
+// )(props => (
+//   <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+//     <Marker position={{ lat: -34.397, lng: 150.644 }} />
+//   </GoogleMap>
+// ));
+//
+// export default ReactGoogleMaps;
