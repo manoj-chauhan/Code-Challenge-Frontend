33bda5e4cd89a58996f8602df0e84705e38fd439
Manoj Chauhan <manoj@kiwitech.com>
2018-05-14 11:45:36 +0530

    code cleanup for development

--- src/Application.js
+++ src/Application.js
@@ -1,55 +1,12 @@
-import React, { Component } from 'react';
-import AppBar from 'material-ui/AppBar';
-import GridListExampleSimple from './GridListExampleSimple';
-import TextFieldExampleSimple from './TextFieldExampleSimple';
-import MyFancyComponent from './MyFancyComponent';
-import TextField from 'material-ui/TextField';
-import {Tabs, Tab} from 'material-ui/Tabs';
-import Slider from 'material-ui/Slider';
-
-
-
-const styles = {
-    headline: {
-        fontSize: 24,
-        paddingTop: 16,
-        marginBottom: 12,
-        fontWeight: 400,
-    },
-};
-
-
-function handleActive(tab) {
-    alert(`A tab with this route property ${tab.props['data-route']} was activated.`);
-}
+import React, {Component} from 'react';
+import PropertyMapComponent from './PropertyMapComponent';
 
 class Application extends Component {
-  render() {
-    return (
-      <div style={{ paddingTop: 56 }}>
-          <AppBar style={{ position: 'fixed', top: 0 }} />
-          <div style={{ position: 'relative'}}>
-              <img style={{ width: '100%' }} src={ require('./images/banner.jpeg') } />
-              <Tabs>
-                  <Tab label="Item One" >
-                      <div>
-                          <GridListExampleSimple/>
-                      </div>
-                  </Tab>
-                  <Tab label="Item Two" >
-                      <div>
-                          <MyFancyComponent/>
-                      </div>
-                  </Tab>
-              </Tabs>
-              <div style={{ position: 'absolute', top: 0, left: 0, margin: 'auto', right: 0, bottom: 0, height: '48px', width: '256px' }}>
-              <TextField hintText="Hint Text"/>
-              </div>
-          </div>
-
-      </div>
-    );
-  }
+    render() {
+        return (
+            <PropertyMapComponent/>
+        );
+    }
 }
 
 export default Application;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Application from './Application';
-import SearchComponent from './SearchComponent';
 import registerServiceWorker from './registerServiceWorker';
 
 const App = () => (
   <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-    <SearchComponent />
+    <Application/>
   </MuiThemeProvider>
 );
 
