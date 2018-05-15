263f2a312d6f2d687bbd5788f2d4456e9aee1935
Manoj Chauhan <manoj@kiwitech.com>
2018-05-15 16:04:36 +0530

    app bar created

--- src/AppBarComponent.js
+++ src/AppBarComponent.js
@@ -1,14 +1,34 @@
 import React from "react"
+import FlatButton from 'material-ui/FlatButton';
 
 const barStyle = {
-        height: 48,
-        backgroundColor: '#20C063'
+    backgroundColor: '#20C063',
+    display:`flex`,
+    justifyContent:`space-between`,
+    height:48
+};
+
+
+
+const signInButtonStyle = {
+    marginTop:6,
+    color:`white`
+};
+
+const logoStyle = {
+    marginTop:6,
 };
 
 export default class AppBarComponent extends React.Component {
     render(){
         return(
-            <div style={barStyle}></div>
+            <div style={barStyle}>
+                <img src={require('./images/logo.png')} style={logoStyle}/>
+                <div>
+                    <FlatButton label="Sign In" style={signInButtonStyle} />
+                    <FlatButton label="Sign Up" style={signInButtonStyle} />
+                </div>
+            </div>
         )
     }
 }
